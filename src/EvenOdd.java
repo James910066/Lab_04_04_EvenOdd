f7a9eadd53142cbeb71189e2b0e0ae34e7aaba17
James910066 <james.dinser@gmail.com>
2022-06-07 20:26:35 -0400

    Add Lab04_04 EvenOdd

---
+++ src/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 4;
+        int modResult = numToExamine % numToExamine;
+
+        System.out.println("The modulo of " + numToExamine + " is " + modResult + " which makes it even");
+    }
+
+}
